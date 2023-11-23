23dbb5cd520343aa16906a0e844e0be19bb5d9c7
José Carlos Rodríguez <jose.rodriguez@grupobusiness.it>
2023-11-22 23:20:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Listado from './components/Listado.js'
+
+const listaObjetos = [
+  { id: 1, propiedad1: 'Valor1A', propiedad2: 'Valor1B', propiedad3: 'Valor1C' },
+  { id: 2, propiedad1: 'Valor2A', propiedad2: 'Valor2B', propiedad3: 'Valor2C' },
+  { id: 3, propiedad1: 'Valor3A', propiedad2: 'Valor3B', propiedad3: 'Valor3C' },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>MY DEMO</h1>
+        <Listado objetos={listaObjetos}></Listado>
       </header>
     </div>
   );
